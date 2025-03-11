2e05657550ddb9f9e6f274399629f7eb5bb47a9f
Aharon-Bruchim <aharon.yesodot@gmail.com>
2025-03-11 15:03:13 +0200

    _redirects

--- ui/src/api/api.ts
+++ ui/src/api/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://website-monitor-48kx.onrender.com/websites",
   baseURL: "https://website-monitor-48kx.onrender.com/websites",
+  // baseURL: "http://localhost:3000/websites",
   headers: {
     "Content-Type": "application/json",
   },
