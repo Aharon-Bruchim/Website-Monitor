4c4c32f265e7c36dc4df5a6fdc4512ca62122fe8
Aharon-Bruchim <aharon.yesodot@gmail.com>
2025-03-11 14:52:12 +0200

    deploy

--- ui/src/api/api.ts
+++ ui/src/api/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "https://website-monitor-48kx.onrender.com/websites",
-  baseURL: "http://localhost:3000/websites",
+  baseURL: "https://website-monitor-48kx.onrender.com/websites",
   headers: {
     "Content-Type": "application/json",
   },
